8d7122025ae3d0c3a1c7a552085da789f528c494
Konstantin Kirchheim <konstantin.kirchheim@ovgu.de>
2021-12-02 15:24:07 +0100

    added tinyimages

--- oodtk/dataset/img/tinyimages.py
+++ oodtk/dataset/img/tinyimages.py
@@ -1,4 +1,12 @@
-class TinyImages:
+import logging
+
+import numpy as np
+from torch.utils.data import Dataset
+
+log = logging.getLogger(__name__)
+
+
+class TinyImages(Dataset):
     """
     The TinyImages dataset is often used as auxiliary OOD training data.
     While it has been removed from the website, downloadable versions can be found on the internet.
@@ -12,4 +20,60 @@ class TinyImages:
 
     """
 
-    pass
+    def __init__(self, datafile, cifar_index_file, transform=None, exclude_cifar=True):
+        self.datafile = datafile
+        self.cifar_index_file = cifar_index_file
+        self.n_images = 79302017  #
+
+        data_file = open(self.datafile, "rb")
+
+        def load_image(idx):
+            data_file.seek(idx * 3072)
+            data = data_file.read(3072)
+            return np.fromstring(data, dtype="uint8").reshape(32, 32, 3, order="F")
+
+        self.load_image = load_image
+        self.offset = 0  # offset index
+
+        self.transform = transform
+        self.exclude_cifar = exclude_cifar
+
+        if exclude_cifar:
+            self.cifar_idxs = []
+            with open(self.cifar_index_file, "r") as idxs:
+                for idx in idxs:
+                    # indices in file take the 80mn database to start at 1, hence "- 1"
+                    self.cifar_idxs.append(int(idx) - 1)
+
+            # hash table option
+            self.cifar_idxs = set(self.cifar_idxs)
+            self.in_cifar = lambda x: x in self.cifar_idxs
+
+    def __getitem__(self, index):
+        index = (index + self.offset) % self.n_images
+
+        if self.exclude_cifar:
+            while self.in_cifar(index):
+                index = np.random.randint(self.n_images)
+
+        while True:
+            try:
+                img = self.load_image(index)
+                if self.transform is not None:
+                    img = self.transform(img)
+
+                return img, -1  # 0 is the class
+            except ValueError as e:
+                # log.warning(f"Failed to read image {index}")
+                index = np.random.randint(self.n_images)
+            except Exception as e:
+                log.warning(f"Failed to read image {index}")
+                log.exception(e)
+                index = np.random.randint(self.n_images)
+
+                if self.exclude_cifar:
+                    while self.in_cifar(index):
+                        index = np.random.randint(self.n_images)
+
+    def __len__(self):
+        return self.n_images
