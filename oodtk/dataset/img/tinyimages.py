3896ce89c674e44fcd195daac52282986500fb31
Konstantin Kirchheim <konstantin.kirchheim@ovgu.de>
2021-12-02 11:06:36 +0100

    added pre-commit hooks

--- oodtk/dataset/img/tinyimages.py
+++ oodtk/dataset/img/tinyimages.py
@@ -1,13 +1,15 @@
-class TinyImages():
+class TinyImages:
     """
-    Even though its use is discouraged, the TinyImages dataset is often used as OOD training data.
+    The TinyImages dataset is often used as auxiliary OOD training data.
     While it has been removed from the website, downloadable versions can be found on the internet.
 
     :see Website: https://groups.csail.mit.edu/vision/TinyImages/
+    :see Archive: https://archive.org/details/80-million-tiny-images-1-of-2
 
     ..  warning::
         The use of this dataset is discouraged by the authors.
         See *Large image datasets: A pyrrhic win for computer vision?*
 
     """
+
     pass
