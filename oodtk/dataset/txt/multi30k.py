2e0972f8152072d86339461cfef774766c3a7b95
Konstantin Kirchheim <konstantin.kirchheim@ovgu.de>
2022-04-04 17:27:08 +0200

    added first text datasets

---
+++ oodtk/dataset/txt/multi30k.py
@@ -0,0 +1,11 @@
+"""
+Used as outlier data in
+* https://github.com/hendrycks/outlier-exposure/blob/master/NLP_classification/eval_OOD_20ng.py
+
+"""
+
+
+class Dataset:
+    """ """
+
+    url = "https://github.com/multi30k/dataset"
