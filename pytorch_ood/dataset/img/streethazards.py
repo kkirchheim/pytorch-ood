f7120c0b07919dffa5fef1093f594466b04ef575
Budhaditya Mukhopadhyay <62721668+BudhaTronix@users.noreply.github.com>
2022-05-03 00:45:01 +0200

    Updated code for street hazard

--- pytorch_ood/dataset/img/streethazards.py
+++ pytorch_ood/dataset/img/streethazards.py
@@ -2,18 +2,15 @@ import logging
 import os
 from os.path import join
 from typing import Any, Callable, Optional, Tuple
-
-import numpy as np
-import scipy.io
 from PIL import Image
-from torchvision.datasets import VisionDataset
-from torchvision.datasets.folder import default_loader
-from torchvision.datasets.utils import check_integrity, download_and_extract_archive
+
+
+from .base import ImageDatasetBase
 
 log = logging.getLogger(__name__)
 
 
-class StreetHazards(VisionDataset):
+class StreetHazards(ImageDatasetBase):
     """
     Benchmark Dataset for Anomaly Segmentation.
 
@@ -23,40 +20,65 @@ class StreetHazards(VisionDataset):
     :see Website: https://github.com/hendrycks/anomaly-seg
     """
 
-    base_folder = "dtd/images/"
-    url = {
-        "test": "https://people.eecs.berkeley.edu/~hendrycks/streethazards_test.tar",
-        "train": "https://people.eecs.berkeley.edu/~hendrycks/streethazards_train.tar",
-    }
-    filename = {"test": "streethazards_test.tar", "train": "streethazards_train.tar"}
-    tgz_md5 = {"test": "8c547c1346b00c21b2483887110bfea7"}
+    subset_list = ["test", "train"]
+
+    base_folder_list = [
+        "test/images/",
+        "train/images/",
+    ]
+    url_list = ["https://people.eecs.berkeley.edu/~hendrycks/streethazards_test.tar","https://people.eecs.berkeley.edu/~hendrycks/streethazards_train.tar"]
+    filename_list = ["streethazards_test.tar", "streethazards_train.tar"]
+    tgz_md5_list = ["8c547c1346b00c21b2483887110bfea7", "cd2d1a8649848afb85b5059d227d2090"]
+
+    def getListOfFiles(self, dirName):
+        # create a list of file and sub directories 
+        # names in the given directory 
+        listOfFile = os.listdir(dirName)
+        allFiles = list()
+        # Iterate over all the entries
+        for entry in listOfFile:
+            # Create full path
+            fullPath = os.path.join(dirName, entry)
+            # If entry is a directory then get the list of files in this directory 
+            if os.path.isdir(fullPath):
+                allFiles = allFiles + self.getListOfFiles(fullPath)
+            else:
+                allFiles.append(fullPath)
+                    
+        return allFiles
 
     def __init__(
         self,
         root: str,
-        train=True,
+        subset: str,
         transform: Optional[Callable] = None,
         target_transform: Optional[Callable] = None,
         download: bool = False,
     ) -> None:
+        if subset not in self.subset_list:
+            raise ValueError(f"Invalid subset: {subset}")
 
-        super(StreetHazards, self).__init__(
+        self.base_folder = self.base_folder_list[self.subset_list.index(subset)]
+        self.url = self.url_list[self.subset_list.index(subset)]
+        self.filename = self.filename_list[self.subset_list.index(subset)]
+        self.tgz_md5 = self.tgz_md5_list[self.subset_list.index(subset)]
+
+        super(ImageDatasetBase, self).__init__(
             root, transform=transform, target_transform=target_transform
         )
+
         if download:
             self.download()
+
         if not self._check_integrity():
             raise RuntimeError(
                 "Dataset not found or corrupted." + " You can use download=True to download it"
             )
 
         self.basedir = os.path.join(self.root, self.base_folder)
-        self.files = []
-        for d in os.listdir(self.basedir):
-            self.files.extend(
-                [join(d, f) for f in os.listdir(join(self.basedir, d)) if not f.startswith(".")]
-            )
-        log.info(f"Found {len(self.files)} texture files.")
+        self.files = self.getListOfFiles(self.basedir)
+
+        
 
     def __getitem__(self, index: int) -> Tuple[Any, Any]:
         """
@@ -66,35 +88,24 @@ class StreetHazards(VisionDataset):
         Returns:
             tuple: (image, target) where target is index of the target class.
         """
-        file, target = self.files[index], -1
+
+        file, target = self.files[index], self.files[index].replace("images", "annotations")
+
         # doing this so that it is consistent with all other datasets
         # to return a PIL Image
-        path = os.path.join(self.root, self.base_folder, file)
-        img = Image.open(path)
+        img = Image.open(file)
+        target = Image.open(target)
+
         if self.transform is not None:
             img = self.transform(img)
+
         if self.target_transform is not None:
             target = self.target_transform(target)
+
         return img, target
 
-    def __len__(self) -> int:
-        return len(self.files)
 
-    def _check_integrity(self) -> bool:
-        root = self.root
-        fpath = os.path.join(root, self.filename)
-        return check_integrity(fpath, self.tgz_md5)
-
-    def download(self) -> None:
-        if self._check_integrity():
-            log.debug("Files already downloaded and verified")
-            return
-
-        download_and_extract_archive(self.url, self.root, filename=self.filename, md5=self.tgz_md5)
-
-    def extra_repr(self) -> str:
-        return "Split: {}".format("Train" if self.train is True else "Test")
-
-    @property
-    def train(self):
-        return False
+obj = StreetHazards(root="C:\\Users\\budha\\Desktop\\temp",download=True, subset='test')
+for i in range(1500):
+    im1, im2 = obj.__getitem__(i)
+    print(im1.size, im2.size)
