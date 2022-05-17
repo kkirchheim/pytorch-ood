4c0206f7f46bfd29115833d4af504e9914c199ee
Budhaditya Mukhopadhyay <budhaditya.mukhopadhyay@st.ovgu.de>
2022-05-17 06:34:21 +0000

    Resolve [#8]

--- test/test_datasets.py
+++ test/test_datasets.py
@@ -4,7 +4,7 @@ from urllib.request import urlopen
 from pytorch_ood.dataset.img.cifar import CIFAR10C, CIFAR100C
 from pytorch_ood.dataset.img.imagenet import ImageNetA, ImageNetC, ImageNetO, ImageNetR
 from pytorch_ood.dataset.img.mnistc import MNISTC
-from pytorch_ood.dataset.txt import Multi30k, NewsGroup20, Reuters52, WikiText2
+from pytorch_ood.dataset.txt import Multi30k, NewsGroup20, Reuters52, WikiText2, WikiText103
 
 
 class TestDatasetAvailability(unittest.TestCase):
@@ -42,6 +42,10 @@ class TestDatasetAvailability(unittest.TestCase):
 
     def test_download_Wiki2(self):
         status = urlopen(WikiText2.url).getcode()
+        self.assertEqual(status, 200)
+
+    def test_download_Wiki103(self):
+        status = urlopen(WikiText103.url).getcode()
         self.assertEqual(status, 200)
 
     def test_download_Reuters52(self):
