347066fdd69072b1325371ac9ebf8040afea7a86
Konstantin Kirchheim <k.kirchheim@gmx.net>
2023-05-31 08:20:43 +0000

    [#33] Resolve "Add Radius to Deep SVDD"

--- tests/loss/test_svdd.py
+++ tests/loss/test_svdd.py
@@ -1,8 +1,10 @@
 import unittest
 
 import torch
+from torch.optim import SGD
 
 from src.pytorch_ood.loss import DeepSVDDLoss, SSDeepSVDDLoss
+from tests.helpers import ClassificationModel
 
 
 class TestDeepSVDD(unittest.TestCase):
@@ -12,6 +14,29 @@ class TestDeepSVDD(unittest.TestCase):
         target = torch.zeros(size=(10,)).long()
         target[5:] = -1
         loss = criterion(logits, target)
+        self.assertIsNotNone(loss)
+        self.assertTrue((loss[5:] == 0).all())
+
+    def test_radius(self):
+        model = ClassificationModel(n_hidden=1024)
+        opti = SGD(model.parameters(), lr=0.01)
+        criterion = DeepSVDDLoss(n_dim=3, radius=1.0, reduction=None)
+
+        x = torch.randn(size=(10, 10))
+        target = torch.zeros(size=(10,)).long()
+
+        target[5:] = -1
+
+        for i in range(1000):
+            loss = criterion(model(x), target)
+
+            loss.mean().backward()
+            opti.zero_grad()
+            opti.step()
+
+        print(loss)
+        print(criterion.distance(model(x)))
+
         self.assertIsNotNone(loss)
         self.assertTrue((loss[5:] == 0).all())
 
