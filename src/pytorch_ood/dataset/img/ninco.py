d0ec37319695398fcc4314cbc1f657fb8a533439
Konstantin Kirchheim <k.kirchheim@gmx.net>
2023-08-19 22:44:01 +0200

    Improved docs for NINCO dataset

--- src/pytorch_ood/dataset/img/ninco.py
+++ src/pytorch_ood/dataset/img/ninco.py
@@ -1,10 +1,7 @@
 import logging
 import os
 from os.path import join
-from typing import Any, Callable, Optional, Tuple
-
-import numpy as np
-from PIL import Image
+from typing import Callable, Optional
 
 from pytorch_ood.dataset.img.base import ImageDatasetBase
 
@@ -13,13 +10,18 @@ log = logging.getLogger(__name__)
 
 class NINCO(ImageDatasetBase):
     """
-    Ninco dataset from the paper
+    NINCO dataset from the paper
     *In or Out? Fixing ImageNet Out-of-Distribution Detection Evaluation*. Contains 5879 OOD images from 64 classes.
-    The images have been varified to be OOD manually.
+    The images have been manually verified as OOD.
 
     Labels are -1 by default.
 
-    :see Paper: `ArXiv <https://arxiv.org/pdf/2306.00826.pdf>`__
+    .. note :: Calculating metrics over the entire dataset will result in slightly different results compared
+        to the original publication, as they calculate metrics over each class individually and
+        report the mean.
+
+
+    :see Paper: `ICML <https://arxiv.org/pdf/2306.00826.pdf>`__
     :see Code: `GitHub <https://github.com/j-cb/NINCO>`__
     :see Download: `Zenodo <https://zenodo.org/record/8013288>`__
 
