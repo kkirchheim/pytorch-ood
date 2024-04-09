2cd9ca4daaaa5b03be0d46a1ad49ca8b8efd9523
tla93 <t.langer93@outlook.de>
2024-04-09 14:07:28 +0200

    run pre_commit2

--- src/pytorch_ood/loss/__init__.py
+++ src/pytorch_ood/loss/__init__.py
@@ -153,6 +153,7 @@ VOS Energy-Based Loss
 .. autoclass:: pytorch_ood.loss.VOSRegLoss
     :members:
 
+
 MCHAD Loss
 ----------------------------------------------
 
@@ -189,7 +190,7 @@ from .ii import IILoss
 from .mchad import MCHADLoss
 from .objectosphere import ObjectosphereLoss
 from .oe import OutlierExposureLoss
-from .vos import VOSRegLoss
 
 # from .triplet import TripletLoss
 from .svdd import DeepSVDDLoss, SSDeepSVDDLoss
+from .vos import VOSRegLoss
