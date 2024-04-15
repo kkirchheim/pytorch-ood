f0381b4fdded9bd1bf0ac8bd41602b2d87b07a74
tla93 <61618820+tla93@users.noreply.github.com>
2024-04-15 12:29:31 +0200

    Energy4 segmentation (#51)
    * Implemented segmentation support for energy loss

--- src/pytorch_ood/loss/__init__.py
+++ src/pytorch_ood/loss/__init__.py
@@ -136,7 +136,7 @@ Energy-Bounded Learning Loss
 
 .. image:: https://img.shields.io/badge/classification-yes-brightgreen?style=flat-square
    :alt: classification badge
-.. image:: https://img.shields.io/badge/segmentation-no-red?style=flat-square
+.. image:: https://img.shields.io/badge/segmentation-yes-brightgreen?style=flat-square
    :alt: classification badge
 
 .. autoclass:: pytorch_ood.loss.EnergyRegularizedLoss
