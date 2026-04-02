601629cb1036f8b374e6443f90c16b281ad184f1
Konstantin Kirchheim <konstantin.kirchheim@ovgu.de>
2026-04-02 14:10:35 +0200

    Added Docs Logo

--- docs/conf.py
+++ docs/conf.py
@@ -86,6 +86,13 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 #
 html_theme = "sphinx_rtd_theme"
 
+html_logo = "_static/pytorch-ood-logo-white.svg"
+
+html_theme_options = {
+    "logo_only": True,
+    "display_version": True,
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
